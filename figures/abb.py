73e726ca5436ee0246c08443e8e6e8776225c869
Jonathan Yong <yongrenjie@gmail.com>
2022-12-31 02:39:15 +0000

    Update ABB figure to use dataset with suppressed artefacts

--- figures/abb.py
+++ figures/abb.py
@@ -3,8 +3,8 @@ from aptenodytes import nmrd, fira
 
 fira()
 
-p = nmrd() / '220722-7c-abbs'
-dss = pg.read(p, range(12001, 12004))
+p = nmrd() / '221209-7c-abbs'
+dss = pg.read(p, range(2001, 2004))
 
 k = 0.8
 fig, axs = pg.subplots2d(1, 3, figsize=(12*k, 4*k))
@@ -14,8 +14,8 @@ c13_bounds = (10, None)
 n15_bounds = (110, 130)
 
 dss[0].stage(ax=axs[0], levels=3.5e4, f1_bounds=c13_bounds, f2_bounds=h1_bounds)
-dss[1].stage(ax=axs[1], levels=2.5e4, f1_bounds=n15_bounds, f2_bounds=h1_bounds)
-dss[2].stage(ax=axs[2], levels=2.5e4, f1_bounds=n15_bounds, f2_bounds=h1_bounds)
+dss[1].stage(ax=axs[1], levels=3.5e4, f1_bounds=n15_bounds, f2_bounds=h1_bounds)
+dss[2].stage(ax=axs[2], levels=3.5e4, f1_bounds=n15_bounds, f2_bounds=h1_bounds)
 
 
 titles = [
